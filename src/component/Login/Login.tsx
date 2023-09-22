40b669f43ba1900f7fe1022f3f3ab78f57f81b32
Rashik <smarashik00@gmail.com>
2023-09-22 12:06:37 +0600

    changed the style

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,81 +1,73 @@
 import React from "react";
-import Lottie from "lottie-react";
-import data from "./login.json";
-import { Link } from "react-router-dom";
+import logo from "../../assets/webtricker-logo-black.jpg";
 
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
   return (
-    <div className="mt-[60px] w-[96%] md:w-[90%] mx-auto pt-3">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="hero h-full changebg">
-          <div className=""></div>
-          <div className="hero-content text-neutral-content">
-            <div className="w-full  h-full">
-              <form className="w-full h-full bg-gradient-to-b from-[#134B67] to-[#101E41] p-3 md:p-20 rounded shadow-2xl">
-                <div className="flex justify-between">
-                  <h2 className="text-2xl font-semibold mb-4 uppercase text-blue-500">
-                    Log In
-                  </h2>
-                  <Link to="/signup">
-                    <h2 className="text-2xl font-semibold mb-4 text-white uppercase hover:text-blue-500">
-                      Sign Up
-                    </h2>
-                  </Link>
-                </div>
+    <div className="mt-[60px] flex justify-center items-center h-screen">
+      <div className="w-96  max-w-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="hero h-full changebg">
+            <div className="hero-content text-neutral-content">
+              <div className="w-96 m-auto h-full flex flex-col justify-center text-center items-center">
+                <form className="w-full h-full p-3 md:p-20 rounded shadow-2xl">
+                  <div className="text-center m-5">
+                    <img
+                      className="w-32 h-24 rounded-full shadow-2xl m-auto"
+                      src={logo}
+                      alt=""
+                    />
+                  </div>
+                  <p className="text-xl my-4"> Welcome Back to WebTricker</p>
+                  <div className="mb-6">
+                    <p className="text-left">Email</p>
+                    <input
+                      type="email"
+                      required
+                      id="Email"
+                      name="Email"
+                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                      placeholder="Your Email"
+                    />
+                  </div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-gray-200 font-medium mb-1"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    required
-                    id="Email"
-                    name="Email"
-                    className="w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                    placeholder="Your Email"
-                  />
-                </div>
+                  <div className="mb-6 relative">
+                    <p className="text-left">Password</p>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      required
+                      className="w-full border rounded py-3 px-4 text-gray-95000 leading-tight focus:outline-none focus:border-blue-500"
+                      placeholder="Your password"
+                    />
+                  </div>
 
-                <div className="mb-4 relative">
-                  <label
-                    htmlFor="password"
-                    className="block text-gray-200 font-medium mb-1"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    required
-                    className="w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                    placeholder="Your password"
-                  />
-                  <p className="absolute top-8 right-5 font-bold cursor-pointer hover:text-blue-500">
-                    Show
-                  </p>
-                </div>
-
-                <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full">
-                  Login
-                </button>
-              </form>
+                  <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
+                    Login
+                  </button>
+                  <div className="my-6 flex justify-between items-center">
+                    <label className="flex items-center">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-5 w-5 text-blue-500"
+                      />
+                      <span className="ml-2 text-xs text-gray-700">
+                        Remember Me
+                      </span>
+                    </label>
+                    <a
+                      href="#"
+                      className="text-xs text-blue-500 hover:underline"
+                    >
+                      Forgot Password?
+                    </a>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="changebg">
-          <Lottie
-            loop={true}
-            autoplay={true}
-            animationData={data}
-            className="w-full h-[40vh] md:h-[80vh]"
-          />
         </div>
       </div>
     </div>
--- src/component/Signup/Signup.tsx
+++ src/component/Signup/Signup.tsx
@@ -1,100 +1,75 @@
 import React from "react";
-import Lottie from "lottie-react";
-import data from "./signup.json";
-import { Link } from "react-router-dom";
+import logo from "../../assets/webtricker-logo-black.jpg";
 
-interface SignUpProps {}
+interface LoginProps {}
 
-const SignUp: React.FC<SignUpProps> = () => {
+const SignUp: React.FC<LoginProps> = () => {
   return (
-    <div className="mt-[60px] w-[96%] md:w-[90%] mx-auto pt-3">
-      <div className="grid grid-cols-1 md:grid-cols-2 flex-col-reverse">
-        <div className="hero h-full changebg">
-          <div className="hero-content text-neutral-content">
-            <div className="">
-              <form className="w-full h-full bg-gradient-to-b from-[#134B67] to-[#101E41] p-3 md:p-20 rounded shadow-2xl">
-                <div className="flex justify-between">
-                  <h2 className="text-2xl font-semibold mb-4 uppercase text-blue-500">
-                    Sign Up
-                  </h2>
-                  <Link to={"/login"}>
-                    <h2 className="text-2xl font-semibold mb-4 text-white uppercase hover:text-blue-500">
-                      Log in
-                    </h2>
-                  </Link>
-                </div>
-                {/* name */}
-                <div className="mb-4">
-                  <label
-                    htmlFor="name"
-                    className="text-gray-200 font-medium mb-3"
-                  >
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                    placeholder="Your Name"
-                  />
-                </div>
+    <div className="mt-[60px] flex justify-center items-center h-screen">
+      <div className="w-96  max-w-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="hero h-full changebg">
+            <div className="hero-content text-neutral-content">
+              <div className="w-96 m-auto h-full flex flex-col justify-center text-center items-center">
+                <form className="w-full h-full p-3 md:p-20 rounded shadow-2xl">
+                  <div className="text-center m-5">
+                    <img
+                      className="w-32 h-24 rounded-full shadow-2xl m-auto"
+                      src={logo}
+                      alt=""
+                    />
+                  </div>
+                  <p className="text-xl my-4"> Welcome to WebTricker</p>
+                  <div className="mb-6">
+                    <p className="text-left">Name</p>
+                    <input
+                      type="text"
+                      required
+                      id="Name"
+                      name="Name"
+                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <p className="text-left">Email</p>
+                    <input
+                      type="email"
+                      required
+                      id="Email"
+                      name="Email"
+                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                      placeholder="Your Email"
+                    />
+                  </div>
 
-                {/* Email */}
-                <div className="mb-4">
-                  <label
-                    htmlFor="email"
-                    className="block text-gray-200 font-medium mb-1"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="text"
-                    id="Email"
-                    name="Email"
-                    className="w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                    placeholder="Your Email"
-                  />
-                </div>
-                {/* password */}
-                <div className="mb-4 relative">
-                  <label
-                    htmlFor="password"
-                    className="block text-gray-200 font-medium mb-1"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                    placeholder="Your password"
-                  />
-                  <p className="absolute top-8 right-5 font-bold cursor-pointer hover:text-blue-500">
-                    show
-                  </p>
-                </div>
+                  <div className="mb-6 relative">
+                    <p className="text-left">Password</p>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      required
+                      className="w-full border rounded py-3 px-4 text-gray-95000 leading-tight focus:outline-none focus:border-blue-500"
+                      placeholder="Your password"
+                    />
+                  </div>
 
-                {/* Add other input fields */}
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full"
-                >
-                  Sign Up
-                </button>
-              </form>
+                  <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
+                    Login
+                  </button>
+                  <div className="my-6 ">
+                    <a
+                      href="#"
+                      className="text-xs text-blue-500 hover:underline"
+                    >
+                      Already Have an account?
+                    </a>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="changebg">
-          <Lottie
-            loop={true}
-            autoplay={true}
-            animationData={data}
-            className="w-full h-[40vh] md:h-[94vh]"
-          />
         </div>
       </div>
     </div>
