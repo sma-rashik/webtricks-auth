7ac910cf7f8f5f9de94c4cbbe587bf2db101ff6d
Rashik <smarashik00@gmail.com>
2023-09-22 15:37:37 +0600

    rspnsv

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -5,64 +5,57 @@ interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
   return (
-    <div className="mt-[60px] flex justify-center items-center h-screen">
-      <div className="w-96  max-w-xl">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="hero h-full changebg">
-            <div className="hero-content text-neutral-content">
-              <div className="w-96 m-auto h-full flex flex-col justify-center text-center items-center">
-                <form className="w-full h-full p-3 md:p-20 rounded shadow-2xl">
-                  <div className="text-center m-5">
-                    <img className="w-20 h-12  m-auto" src={logo} alt="" />
-                  </div>
-                  <p className="text-xl my-4"> Welcome Back to WebTricker</p>
-                  <div className="mb-6">
-                    <p className="text-left">Email</p>
-                    <input
-                      type="email"
-                      required
-                      id="Email"
-                      name="Email"
-                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                      placeholder="Your Email"
-                    />
-                  </div>
+    <div className="mt-8 sm:mt-16 flex justify-center items-center">
+      <div className="w-full sm:w-11/12 md:w-3/4 lg:w-1/2 xl:w-1/3">
+        <div className="bg-white shadow-md rounded p-4">
+          <div className="text-center">
+            <img className="w-20 h-12 m-auto" src={logo} alt="" />
+            <h1 className="text-xl font-semibold mt-4">
+              Welcome Back to WebTricker
+            </h1>
+          </div>
 
-                  <div className="mb-6 relative">
-                    <p className="text-left">Password</p>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      required
-                      className="w-full border rounded py-3 px-4 text-gray-95000 leading-tight focus:outline-none focus:border-blue-500"
-                      placeholder="Your password"
-                    />
-                  </div>
-
-                  <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
-                    Login
-                  </button>
-                  <div className="my-6 flex justify-between items-center">
-                    <label className="flex items-center">
-                      <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-blue-500"
-                      />
-                      <span className="ml-2 text-xs text-gray-700">
-                        Remember Me
-                      </span>
-                    </label>
-                    <a
-                      href="#"
-                      className="text-xs text-blue-500 hover:underline"
-                    >
-                      Forgot Password?
-                    </a>
-                  </div>
-                </form>
-              </div>
+          <form className="mt-6">
+            <div className="mb-4">
+              <p className="text-left text-gray-600 font-medium mb-1">Email</p>
+              <input
+                type="email"
+                required
+                id="Email"
+                name="Email"
+                className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                placeholder="Your Email"
+              />
             </div>
+            <div className="mb-4 relative">
+              <p className="text-left text-gray-600 font-medium mb-1">
+                Password
+              </p>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required
+                className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                placeholder="Your password"
+              />
+            </div>
+            <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
+              Login
+            </button>
+          </form>
+
+          <div className="my-6 flex justify-between items-center">
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                className="form-checkbox h-5 w-5 text-blue-500"
+              />
+              <span className="ml-2 text-xs text-gray-700">Remember Me</span>
+            </label>
+            <a href="#" className="text-xs text-blue-500 hover:underline">
+              Forgot Password?
+            </a>
           </div>
         </div>
       </div>
--- src/component/Signup/Signup.tsx
+++ src/component/Signup/Signup.tsx
@@ -1,70 +1,73 @@
 import React from "react";
 import logo from "../../assets/webtricker-logo-black.jpg";
 
-interface LoginProps {}
+interface SignUpProps {}
 
-const SignUp: React.FC<LoginProps> = () => {
+const SignUp: React.FC<SignUpProps> = () => {
   return (
-    <div className="mt-[60px] flex justify-center items-center h-screen">
-      <div className="w-96  max-w-xl">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="hero h-full changebg">
-            <div className="hero-content text-neutral-content">
-              <div className="w-96 m-auto h-full flex flex-col justify-center text-center items-center">
-                <form className="w-full h-full p-3 md:p-20 rounded shadow-2xl">
-                  <div className="text-center m-5">
-                    <img className="w-20 h-12 m-auto" src={logo} alt="" />
-                  </div>
-                  <p className="text-xl my-4"> Welcome to WebTricker</p>
-                  <div className="mb-6">
-                    <p className="text-left">Name</p>
-                    <input
-                      type="text"
-                      required
-                      id="Name"
-                      name="Name"
-                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                      placeholder="Your Name"
-                    />
-                  </div>
-                  <div className="mb-6">
-                    <p className="text-left">Email</p>
-                    <input
-                      type="email"
-                      required
-                      id="Email"
-                      name="Email"
-                      className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                      placeholder="Your Email"
-                    />
-                  </div>
+    <div className="mt-8 sm:mt-16 flex justify-center items-center">
+      <div className="w-full sm:w-11/12 md:w-3/4 lg:w-1/2 xl:w-1/3">
+        <div className="bg-white shadow-md rounded p-4">
+          <div className="text-center">
+            <img className="w-20 h-12 m-auto" src={logo} alt="" />
+            <h1 className="text-xl font-semibold mt-4">
+              Welcome to WebTricker
+            </h1>
+          </div>
 
-                  <div className="mb-6 relative">
-                    <p className="text-left">Password</p>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      required
-                      className="w-full border rounded py-3 px-4 text-gray-95000 leading-tight focus:outline-none focus:border-blue-500"
-                      placeholder="Your password"
-                    />
-                  </div>
-
-                  <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
-                    Login
-                  </button>
-                  <div className="my-6 ">
-                    <a
-                      href="#"
-                      className="text-xs text-blue-500 hover:underline"
-                    >
-                      Already Have an account?
-                    </a>
-                  </div>
-                </form>
-              </div>
+          <form className="mt-6">
+            <div className="mb-4">
+              <p className="text-left text-gray-600 font-medium mb-1">Name</p>
+              <input
+                type="text"
+                required
+                id="Name"
+                name="Name"
+                className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                placeholder="Your Name"
+              />
             </div>
+            <div className="mb-4">
+              <label
+                htmlFor="Email"
+                className="block text-gray-600 font-medium mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                required
+                id="Email"
+                name="Email"
+                className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                placeholder="Your Email"
+              />
+            </div>
+            <div className="mb-4 relative">
+              <p className=" text-left text-gray-600 font-medium mb-1">
+                Password
+              </p>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required
+                className="w-full border rounded py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                placeholder="Your password"
+              />
+            </div>
+            <button className="bg-gray-950 hover:bg-blue-600 text-white font-semibold py-3 px-6 rounded w-full">
+              Sign Up
+            </button>
+          </form>
+
+          <div className="mt-4 text-center">
+            <p className="text-sm text-gray-600">
+              Already have an account?{" "}
+              <a href="#" className="text-blue-500 hover:underline">
+                Log in
+              </a>
+            </p>
           </div>
         </div>
       </div>
